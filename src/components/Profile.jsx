1d19e8e82bfe509c5dcc6f918bd3f2b849685915
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2024-08-03 14:52:32 +0430

    remove img

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,12 +4,10 @@ import styles from './Profile.module.scss'
 const Profile = () => {
   return (
     <div className={`${styles.profile}`}>
-    <img src="/images/Abbas.jpg" alt="" />
     <h1>
         <span>&#129505; Abbas Akhavan &#129505;</span>
         <span>Professional FrontEnd Developer &#128526;</span>
     </h1>
-    <span>Coming soon &#128170;</span>
     </div>
   )
 }
