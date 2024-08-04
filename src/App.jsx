87f94ae019271be27dba9a56077790fd70d1b267
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2024-08-04 05:09:53 +0430

    new portfolio start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import styles from './App.scss';
-import Profile from './components/Profile';
+import { useState } from "react";
+
 function App() {
-  
+  const [darkMode,setDarkMode] = useState(true);
   return (
-    <Profile />
-  )
+    <>
+      <main className={`transition-all duration-200 ${darkMode ? 'dark' : ''}`}>
+        <div className="bg-slate-300 pb-96 dark:bg-slate-900">
+          <aside className="fixed h-[calc(100vh-32px)] w-60 border border-slate-600 rounded-2xl top-[16px] left-4"></aside>
+          <main>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+            <div className="h-52"></div>
+          </main>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode : 'selector',
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
