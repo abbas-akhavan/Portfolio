8fb71c539e14575d907e1abdf84dc9fb5f3518a6
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2024-08-07 00:58:05 +0430

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,54 @@ import { useState } from "react";
 import gsap from "gsap";
 import NavBar from "./components/NavBar";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import { FaRegListAlt } from "react-icons/fa";
+import { FaRegLightbulb } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { FaBookOpen } from "react-icons/fa";
+import { FaRegFile } from "react-icons/fa";
 
 function App() {
-  const [darkMode,setDarkMode] = useState(true);
-  // useGSAP(()=>{
-  //   gsap.to('#logo', {
-  //     duration: 1, 
-  //     scrambleText: {
-  //       text: "THIS IS NEW TEXT", 
-  //       chars: "XO", 
-  //       revealDelay: 0.5, 
-  //       speed: 0.3, 
-  //       newClass: "myClass"
-  //     }
-  //   }); 
-  // })
+  const [darkMode, setDarkMode] = useState(true);
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const summaryTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#summary",
+        start: "top 70%",
+        end: "bottom+=50% bottom",
+        scrub : 1,
+        markers : true
+      },
+    });
+
+    summaryTL.from("#summary >div", {
+      xPercent: -20,
+      opacity: 0,
+    });
+  });
   return (
     <>
-    <NavBar/>
-      <main>
-        
+      <NavBar />
+      <main className="h-[2000px] w-[60%] pt-[1000px] mx-auto">
+        <section id="summary">
+          <div>
+            <div className="flex gap-2 text-white font-bold items-center">
+              <FaRegListAlt /> Summary
+            </div>
+            <div className="grid  gap-6 grid-cols-[1fr_3fr]">
+              <img src="/images/Abbas.jpg" alt="" />
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+                totam odit qui distinctio omnis aut. Qui laudantium et quaerat
+                minima molestias modi blanditiis ratione, asperiores, dolorum
+                illum exercitationem pariatur reiciendis.
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,53 +1,61 @@
 import gsap from "gsap";
-import { useGSAP } from '@gsap/react';
-import React, { useEffect, useRef } from 'react';
+import { useGSAP } from "@gsap/react";
+import { FaRegListAlt } from "react-icons/fa";
+import { FaRegLightbulb } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { FaBookOpen } from "react-icons/fa";
+import { FaRegFile } from "react-icons/fa";
+import React, { useEffect, useRef } from "react";
 
 
 const NavBar = () => {
-    useGSAP(()=>{
-
-      const navTL = gsap.timeline();
-      navTL.from('#navbar' , {
-        duration : 1.5,
-        y : -50,
-        opacity : 0,
-        ease : "elastic.out(1,0.5)"
+  useGSAP(() => {
+    const navTL = gsap.timeline();
+    navTL
+      .from("#navbar", {
+        duration: 1.5,
+        y: -50,
+        opacity: 0,
+        ease: "elastic.out(1,0.5)",
       })
-.from('#menu li' , {
-        duration : .5,
-        opacity : 0,
-        yPercent : -100,
-        stagger : .05,
-      })
-      .from('#logo' , {
-        duration : 1.5,
-        scale : 0,
-        opacity : 0,
-        ease : "elastic.out(1,0.5)"
-      })
-      
-
-      })
+      .from("#menu li", {
+        duration: 0.5,
+        opacity: 0,
+        yPercent: -100,
+        stagger: 0.05,
+      },'-=.5')
+      .from("#logo", {
+        duration: 1.5,
+        scale: 0,
+        opacity: 0,
+        ease: "elastic.out(1,0.5)",
+      });
+  });
   return (
-    <nav id="navbar" className="fixed top-10 left-1/2 -translate-x-1/2 w-[60%] rounded-xl bg-blurBgColor  backdrop-blur-sm shadow-lg flex gap-8 p-4">
-      <span id="logo" className="text-white font-black">ABBAS AKHAVAN</span>
-      <ul id="menu" className="flex text-white gap-14">
-        <li>Summary</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Education</li>
-        <li>My Resume</li>
+    <nav
+      id="navbar"
+      className="fixed top-10 left-1/2 -translate-x-1/2 w-[60%] rounded-xl bg-blurBgColor  backdrop-blur-sm shadow-lg flex gap-8 p-4"
+    >
+      <span id="logo" className="text-white font-black">
+        ABBAS AKHAVAN
+      </span>
+      <ul id="menu" className="flex text-white gap-14 grow *:cursor-pointer *:flex *:items-center *:gap-1">
+        <li ><FaRegListAlt /> Summary</li>
+        <li ><FaRegLightbulb /> Skills</li>
+        <li ><FaLaptopCode /> Projects</li>
+        <li ><FaBookOpen /> Education</li>
+        <a href="#" className=" ml-auto"><FaRegFile /> My Resume</a>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
 
 // scrambleText: {
-//   text: "THIS IS NEW TEXT", 
-//   chars: "XO", 
-//   revealDelay: 0.5, 
-//   speed: 0.3, 
+//   text: "THIS IS NEW TEXT",
+//   chars: "XO",
+//   revealDelay: 0.5,
+//   speed: 0.3,
 //   newClass: "myClass"
 // }
